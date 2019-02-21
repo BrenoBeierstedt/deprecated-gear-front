26fb62e069547eafc642084d9b1d481ddf9a9125
Breno <breno.beierdtedt@hotmail.com>
2019-02-20 23:49:37 -0300

    component adjustments

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -1,24 +1,31 @@
 
+
+
+
 import React from "react"
 import { Route, Switch,Redirect,  } from "react-router-dom"
 import Layout from "../components/pageComponents/layout";
 import {isAuthenticated} from "../auth";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-<Layout>
+    <div>
+
     <Route
         {...rest}
         render={props => (
                 isAuthenticated() ?(
-
+<div>
+    <Layout>
                         <Component {...props}/>
-
+    </Layout>
+</div>
                     )
                     :(
-                        <Redirect exact to={{ pathname: '/',search:'?msg=É necessário estar logado para acessar a página',state: {from: props.location}}} />
+                        <Redirect exact to={{ pathname: '/login',search:'?msg=É necessário estar logado para acessar a página',state: {from: props.location}}} />
                     )
             )}/>
 
-</Layout>   );
+
+    </div>);
 
             export default PrivateRoute;
