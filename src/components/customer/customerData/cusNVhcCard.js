3355cd346be7f586688e4d4eef3f003827a35a73
Breno <breno.beierdtedt@hotmail.com>
2019-02-23 16:01:29 -0300

    card for customer and vehicle data

---
+++ src/components/customer/customerData/cusNVhcCard.js
@@ -0,0 +1,26 @@
+
+
+import React, {Component} from "react";
+
+export default class cusNVhcCard extends Component {
+
+
+
+    render(){
+        return(
+<div className="col-md-13">
+    <div className="card p-30">
+        <div className="media">
+
+            <div className="media-left meida media-middle">
+                <span><i className="fa fa-usd f-s-40 color-primary"></i></span>
+            </div>
+            <div className="media-body media-text-right">
+                <h2>568120</h2>
+                <p className="m-b-0">CARD PARA RELATÓRIO MANUSCRITO</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+        )}}
